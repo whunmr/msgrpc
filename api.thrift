a427bd28f773bd4450f97662223e6b138c933194
whunmr <whunmr@gmail.com>
2017-03-06 15:44:00 +0800

    init

---
+++ api.thrift
@@ -0,0 +1,17 @@
+namespace cpp org.example.msif.demo
+
+struct RequestData {
+        1: i32 pet_id,
+}
+
+struct ResponseData {
+        1: i32    pet_id,
+        2: string pet_name,
+        3: i32    pet_weight,
+}
+
+service Petstore {
+        ResponseData get_pet_info(1: RequestData req),
+}
+
+

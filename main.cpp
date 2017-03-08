88217f6becb7c3d99cfb4bc7be0ef493ddfe6506
whunmr <whunmr@gmail.com>
2017-03-08 17:51:27 +0800

    add simple type support (int32_t, std::string) to let macros to expand to declarationa nd definition of struct, instead using thrift generator

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 #include <thrift/transport/TSocket.h>
 #include <thrift/transport/TTransportUtils.h>
 
+#include <thrift/TToString.h>
+
 using namespace std;
 using namespace apache::thrift;
 using namespace apache::thrift::protocol;
@@ -96,23 +98,24 @@ struct DummyParent {};
 //    bool pet_name :1;
 //    bool pet_weight :1;
 //} _ResponseData__isset;
-#define ___expand_isset_init_list(fid_, fname_, ftype_)       , fname_(false)
-#define ___expand_isset_field(fid_, fname_, ftype_)           bool fname_ :1;
+#define ___expand_isset_init_list(fid_, fname_, ftype_, ...)       , fname_(false)
+#define ___expand_isset_field(fid_, fname_, ftype_, ...)           bool fname_ :1;
 
 #define ___declare_struct_isset(struct_name_) \
     typedef struct _##struct_name_##__isset : DummyParent<_##struct_name_##__isset> { \
-        _##struct_name_##__isset() : DummyParent<_##struct_name_##__isset>() ___apply_expand(struct_name_, ___expand_isset_init_list) {} \
-        ___apply_expand(struct_name_, ___expand_isset_field) \
+        _##struct_name_##__isset() : DummyParent<_##struct_name_##__isset>()\
+             ___apply_expand(struct_name_, ___expand_isset_init_list, struct_name_) {} \
+        ___apply_expand(struct_name_, ___expand_isset_field, struct_name_) \
     } _##struct_name_##__isset;
 
 ////////////////////////////////////////////////////////////////////////////////
-#define ___expand_struct_init_list(fid_, fname_, ftype_)    ,fname_(ftype_())
-#define ___expand_struct_field(fid_, fname_, ftype_)        ftype_ fname_;
+#define ___expand_struct_init_list(fid_, fname_, ftype_, ...)    ,fname_(ftype_())
+#define ___expand_struct_field(fid_, fname_, ftype_, ...)        ftype_ fname_;
 
-#define ___expand_declare_set_field_method(fid_, fname_, ftype_) \
+#define ___expand_declare_set_field_method(fid_, fname_, ftype_, ...) \
   void __set_##fname_(const ftype_& val);
 
-#define ___expand_field_compare_statement(fid_, fname_, ftype_) \
+#define ___expand_field_compare_statement(fid_, fname_, ftype_, ...) \
   if (!(fname_ == rhs.fname_)) return false;
 
 #define ___declare_struct_self(struct_name_) \
@@ -121,15 +124,16 @@ struct DummyParent {};
     \
     struct_name_(const struct_name_&);\
     struct_name_& operator=(const struct_name_&);\
-    struct_name_() : DummyParent<struct_name_>() ___apply_expand(struct_name_, ___expand_struct_init_list) {} \
+    struct_name_() : DummyParent<struct_name_>()\
+      ___apply_expand(struct_name_, ___expand_struct_init_list, struct_name_) {} \
     virtual ~struct_name_() throw();\
-    ___apply_expand(struct_name_, ___expand_struct_field)\
+    ___apply_expand(struct_name_, ___expand_struct_field, struct_name_)\
     \
     _##struct_name_##__isset __isset;\
     \
-    ___apply_expand(struct_name_, ___expand_declare_set_field_method)\
+    ___apply_expand(struct_name_, ___expand_declare_set_field_method, struct_name_)\
     bool operator == (const struct_name_ & rhs) const {\
-      ___apply_expand(struct_name_, ___expand_field_compare_statement);\
+      ___apply_expand(struct_name_, ___expand_field_compare_statement, struct_name_)\
       return true;\
     }\
     \
@@ -144,13 +148,177 @@ struct DummyParent {};
 
 ////////////////////////////////////////////////////////////////////////////////
 #define ___expand_define_set_field_method(fid_, fname_, ftype_, struct_name_) \
-    void struct_name_::__set_##fname_(const ftype_& val) { this->fname_ = val;}
+    void struct_name_::__set_##fname_(const ftype_& val) {\
+      this->fname_ = val;\
+    }
+
+//TODO: add all the types
+template<typename T> struct TTypeT {
+    enum {value = ::apache::thrift::protocol::T_VOID};
+    static uint32_t  read(::apache::thrift::protocol::TProtocol* iprot, T& t) { assert(0); }
+    static uint32_t write(::apache::thrift::protocol::TProtocol* oprot, T& t) { assert(0); }
+};
+
+template<> struct TTypeT<bool> {
+    enum {value = ::apache::thrift::protocol::T_BOOL};
+    static uint32_t read(::apache::thrift::protocol::TProtocol* iprot, bool& t) {
+        return iprot->readBool(t);
+    }
+    static uint32_t write(::apache::thrift::protocol::TProtocol* oprot, const bool t) {
+        return oprot->writeBool(t);
+    }
+};
+
+template<> struct TTypeT<int32_t> {
+    enum {value = ::apache::thrift::protocol::T_I32};
+    static uint32_t read(::apache::thrift::protocol::TProtocol* iprot, int32_t& t) {
+        return iprot->readI32(t);
+    }
+    static uint32_t write(::apache::thrift::protocol::TProtocol* oprot, const int32_t t) {
+        return oprot->writeI32(t);
+    }
+};
+
+template<> struct TTypeT<std::string> {
+    enum {value = ::apache::thrift::protocol::T_STRING};
+    static uint32_t read(::apache::thrift::protocol::TProtocol* iprot, std::string& t) {
+        return iprot->readString(t);
+    }
+    static uint32_t write(::apache::thrift::protocol::TProtocol* oprot, const std::string& t) {
+        return oprot->writeString(t);
+    }
+};
+
+/*enum TType {
+    T_VOID       = 1,
+    T_BOOL       = 2,
+    T_BYTE       = 3,
+    T_I08        = 3,
+    T_I16        = 6,
+    T_I32        = 8,
+    T_U64        = 9,
+    T_I64        = 10,
+    T_DOUBLE     = 4,
+    T_STRING     = 11,
+    T_UTF7       = 11,
+    T_STRUCT     = 12,
+    T_MAP        = 13,
+    T_SET        = 14,
+    T_LIST       = 15,
+    T_UTF8       = 16,
+    T_UTF16      = 17
+};*/
+
+#define ___expand_read_field_case(fid_, fname_, ftype_, ...) \
+  case fid_:\
+    if (ftype == TTypeT<ftype_>::value) {\
+      xfer +=  TTypeT<ftype_>::read(iprot, this->fname_);\
+      this->__isset.fname_ = true;\
+    } else {\
+      xfer += iprot->skip(ftype);\
+    }\
+    break;
+
+#define ___define_read(struct_name_) \
+    uint32_t struct_name_::read(::apache::thrift::protocol::TProtocol* iprot) {\
+      apache::thrift::protocol::TInputRecursionTracker tracker(*iprot);\
+      uint32_t xfer = 0;\
+      std::string fname;\
+      ::apache::thrift::protocol::TType ftype;\
+      int16_t fid;\
+      \
+      xfer += iprot->readStructBegin(fname);\
+      \
+      using ::apache::thrift::protocol::TProtocolException;\
+      \
+      \
+      while (true)\
+      {\
+        xfer += iprot->readFieldBegin(fname, ftype, fid);\
+        if (ftype == ::apache::thrift::protocol::T_STOP) {\
+          break;\
+        }\
+        switch (fid)\
+        {\
+          ___apply_expand(struct_name_, ___expand_read_field_case, struct_name_) \
+          default:\
+            xfer += iprot->skip(ftype);\
+            break;\
+        }\
+        xfer += iprot->readFieldEnd();\
+      }\
+      \
+      xfer += iprot->readStructEnd();\
+      \
+      return xfer;\
+    }
+
+
+#define ___expand_write_field(fid_, fname_, ftype_, ...) \
+    xfer += oprot->writeFieldBegin(#fname_, (TType)TTypeT<ftype_>::value, fid_);\
+    xfer += TTypeT<ftype_>::write(oprot, this->fname_); \
+    xfer += oprot->writeFieldEnd();
+
+#define ___define_write(struct_name_) \
+uint32_t struct_name_::write(::apache::thrift::protocol::TProtocol* oprot) const {\
+  uint32_t xfer = 0;\
+  apache::thrift::protocol::TOutputRecursionTracker tracker(*oprot);\
+  xfer += oprot->writeStructBegin(#struct_name_);\
+  \
+  ___apply_expand(struct_name_, ___expand_write_field, struct_name_) \
+  \
+  xfer += oprot->writeFieldStop();\
+  xfer += oprot->writeStructEnd();\
+  return xfer;\
+}
+
+#define ___expand_swap_field(fid_, fname_, ftype_, ...) \
+    swap(a.fname_, b.fname_);
+
+#define ___define_swap(struct_name_) \
+    void swap(struct_name_ &a, struct_name_ &b) {\
+        using ::std::swap;\
+        ___apply_expand(struct_name_, ___expand_swap_field, struct_name_)\
+        swap(a.__isset, b.__isset);\
+    }
+
+
+#define ___expand_assign_field(fid_, fname_, ftype_, ...) \
+    fname_ = other0.fname_;
+
+#define ___define_copy_constructor(struct_name_) \
+    struct_name_::struct_name_(const struct_name_& other0) {\
+      ___apply_expand(struct_name_, ___expand_assign_field, struct_name_)\
+      __isset = other0.__isset;\
+    }
+
+#define ___define_assign_constructor(struct_name_) \
+    ResponseData& ResponseData::operator=(const ResponseData& other0) {\
+        ___apply_expand(struct_name_, ___expand_assign_field, struct_name_)\
+        __isset = other0.__isset;\
+        return *this;\
+    }
+
+#define ___expand_print_out_field(fid_, fname_, ftype_, ...) \
+    out << ", "#fname_"=" << to_string(fname_);
+
+#define ___define_print_to(struct_name_) \
+    void struct_name_::printTo(std::ostream& out) const {\
+        using ::apache::thrift::to_string;\
+        out << #struct_name_"(";\
+        ___apply_expand(struct_name_, ___expand_print_out_field, struct_name_)\
+        out << ")";\
+    }\
 
 #define ___define_struct_self(struct_name_) \
     struct_name_::~struct_name_() throw() {} \
-    \
-    ___apply_expand(struct_name_, ___expand_define_set_field_method, struct_name_);\
-    \
+    ___apply_expand(struct_name_, ___expand_define_set_field_method, struct_name_)\
+    ___define_read(struct_name_)\
+    ___define_write(struct_name_)\
+    ___define_swap(struct_name_)\
+    ___define_copy_constructor(struct_name_)\
+    ___define_assign_constructor(struct_name_)\
+    ___define_print_to(struct_name_)
 
 ////////////////////////////////////////////////////////////////////////////////
 #define ___declare_struct(struct_name_)   \
@@ -193,10 +361,13 @@ int main() {
         ___foo.pet_id = ___foo.pet_weight = 0;
         ___foo.pet_name = "aa";
 
-        if (ThriftDecoder::decode(___foo, pbuf, len)) {
-            cout << ___foo.pet_id << endl;
-            cout << ___foo.pet_name << endl;
-            cout << ___foo.pet_weight << endl;
+
+        msgrpc_demo::ResponseData ___bar;
+
+        if (ThriftDecoder::decode(___bar, pbuf, len)) {
+            cout << ___bar.pet_id << endl;
+            cout << ___bar.pet_name << endl;
+            cout << ___bar.pet_weight << endl;
         }
     }
 

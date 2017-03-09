6d531657068e037b9af408331e280c77c2b4afc6
whunmr <whunmr@gmail.com>
2017-03-09 10:32:56 +0800

    refactor

--- main.cpp
+++ main.cpp
@@ -483,22 +483,22 @@ namespace msgrpc_demo {
 
     ___def_struct(EmbeddedStruct);
 
-    #define ___fields_of_struct___ResponseData(_, ...)         \
-      _(1, pet_id,           int32_t,                        __VA_ARGS__)\
-      _(2, pet_name,         std::string,                    __VA_ARGS__)\
-      _(3, pet_weight,       int32_t,                        __VA_ARGS__)\
-      _(4, pet_i8_value,     int8_t,                         __VA_ARGS__)\
-      _(5, pet_i16_value,    int16_t,                        __VA_ARGS__)\
-      _(6, pet_i64_value,    int64_t,                        __VA_ARGS__)\
-      _(7, pet_double_value, double,                         __VA_ARGS__)\
-      _(8, pet_bool_value,   bool,                           __VA_ARGS__)\
-      _(9, pet_binary_value, binary,                         __VA_ARGS__)\
-      _(10, pet_embedded_struct, EmbeddedStruct,             __VA_ARGS__)\
-      _(11, pet_list_i32,    std::vector<int32_t>,           __VA_ARGS__)\
-      _(12, pet_list_of_struct, std::vector<EmbeddedStruct>, __VA_ARGS__)\
-      _(13, pet_list_of_bool, std::vector<bool>,             __VA_ARGS__)\
-      _(14, pet_set_of_i32, std::set<int32_t>,               __VA_ARGS__)\
-      _(15, pet_set_of_struct, std::set<EmbeddedStruct>,     __VA_ARGS__)
+    #define ___fields_of_struct___ResponseData(_, ...)                    \
+      _(1, pet_id,               int32_t,                     __VA_ARGS__)\
+      _(2, pet_name,             std::string,                 __VA_ARGS__)\
+      _(3, pet_weight,           int32_t,                     __VA_ARGS__)\
+      _(4, pet_i8_value,         int8_t,                      __VA_ARGS__)\
+      _(5, pet_i16_value,        int16_t,                     __VA_ARGS__)\
+      _(6, pet_i64_value,        int64_t,                     __VA_ARGS__)\
+      _(7, pet_double_value,     double,                      __VA_ARGS__)\
+      _(8, pet_bool_value,       bool,                        __VA_ARGS__)\
+      _(9, pet_binary_value,     binary,                      __VA_ARGS__)\
+      _(10, pet_embedded_struct, EmbeddedStruct,              __VA_ARGS__)\
+      _(11, pet_list_i32,        std::vector<int32_t>,        __VA_ARGS__)\
+      _(12, pet_list_of_struct,  std::vector<EmbeddedStruct>, __VA_ARGS__)\
+      _(13, pet_list_of_bool,    std::vector<bool>,           __VA_ARGS__)\
+      _(14, pet_set_of_i32,      std::set<int32_t>,           __VA_ARGS__)\
+      _(15, pet_set_of_struct,   std::set<EmbeddedStruct>,    __VA_ARGS__)
 
 ___def_struct(ResponseData);
 

f71075db16c0995f67d8a4f13ee08d6c97c4ea06
whunmr <whunmr@gmail.com>
2017-04-20 17:52:50 +0800

    add service_y interface impl

--- demo_services/service_y/service_y_main.cpp
+++ demo_services/service_y/service_y_main.cpp
@@ -3,6 +3,7 @@
 #include <api/export/y_api_struct_declare.h>
 
 int main() {
-    std::cout << "service_y_main" << std::endl;
+    std::cout << "[service_start_up] service_y_main" << std::endl;
+
     return 0;
 }
--- demo_services/service_y/src/api/export/y_api_interface_impl_define.cpp
+++ demo_services/service_y/src/api/export/y_api_interface_impl_define.cpp
@@ -2,3 +2,15 @@
 
 #include <msgrpc/api/api_interface_impl_define.h>
 #include <api/export/service_y.interface>
+
+namespace service_y {
+
+    msgrpc::Cell<YRsp>* IY_impl::___f1m1(const YReq& req) {
+        return msgrpc::call_sync_iface_impl<YRsp>(
+            [&req](YRsp &rsp) {
+                rsp.__set_yrspa(req.yreqa * 2);
+            }
+        );
+    }
+
+} //namespace service_y

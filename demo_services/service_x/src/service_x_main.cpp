8b44b69dc3a366508fdc14f6829dd0046ba9a40a
whunmr <whunmr@gmail.com>
2017-04-21 13:12:06 +0800

    remove performance test code

--- demo_services/service_x/src/service_x_main.cpp
+++ demo_services/service_x/src/service_x_main.cpp
@@ -10,10 +10,6 @@ DEFINE_SI(SI_call_y_f1m1, YReq, YRsp) {
     return IY(ctxt).___f1m1(req);
 }
 
-std::atomic_uint count {0};
-
-void rpc_main();
-
 void testcase_0000() {
     YReq yreq;
     yreq.yreqa = 100;
@@ -22,25 +18,14 @@ void testcase_0000() {
 
     if (rsp_cell != nullptr) {
         derive_final_action([](msgrpc::Cell<YRsp>& r) {
-            //std::__1::cout << "value of result:" << r.value().yrspa << std::__1::endl;
-            ++count;
-            if (count % 100 == 0) {
-                std::cout << count << std::endl;
-            }
-            if (count % 3000 == 0) {
-                rpc_main();
-            }
+            std::__1::cout << "value of result:" << r.value().yrspa << std::__1::endl;
         }, rsp_cell);
     }
-
-    //std::this_thread::sleep_for(std::chrono::milliseconds(100));
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 void rpc_main() {
-    for (int j = 0; j < 3000; ++j){
-        testcase_0000();
-    }
+    testcase_0000();
 }
 
 ////////////////////////////////////////////////////////////////////////////////

8b5242471c420d51b3642031796579075fe8bcfb
whunmr <whunmr@gmail.com>
2017-05-16 16:32:44 +0800

    implement service version filter to skip first service instances if service has multiple instances

--- demo_services/service_x/src/service_discovery/service_version_filter.cpp
+++ demo_services/service_x/src/service_discovery/service_version_filter.cpp
@@ -7,8 +7,21 @@ ServiceVersionFilter::ServiceVersionFilter() {
 }
 
 msgrpc::instance_vector_t ServiceVersionFilter::filter_service(const std::string& service_name, const msgrpc::instance_vector_t& instances) {
-    ___log_debug("hit service filter:  we can filter service instances by version here.");
-    return instances;
+    msgrpc::instance_vector_t iv;
+
+    if (instances.size() > 1) {
+        auto iter = instances.begin();
+        ++iter;
+
+        iv.insert(iv.end(), iter, instances.end());
+    } else {
+        iv.insert(iv.end(), instances.begin(), instances.end());
+    }
+
+    ___log_debug("hit service version filter:  we can filter service instances by version here. "\
+                 "instances size before filter: %d, after filter %d", instances.size(), iv.size());
+
+    return iv;
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////

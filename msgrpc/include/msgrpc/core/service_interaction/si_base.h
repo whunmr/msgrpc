6f808381555fd5d72c8ed27b95f7031c0434a3f9
whunmr <whunmr@gmail.com>
2017-05-17 20:07:22 +0800

    efactor all si define using run_si

--- msgrpc/include/msgrpc/core/service_interaction/si_base.h
+++ msgrpc/include/msgrpc/core/service_interaction/si_base.h
@@ -7,25 +7,6 @@
 
 namespace msgrpc {
 
-    template<typename REQ, typename RSP, typename SERVICE_RESOLVER = DefaultServiceResolver>
-    struct SIBase { /*SI is short for service interaction*/
-        msgrpc::Cell<RSP> *run(const REQ &req) {
-            msgrpc::RpcContext *ctxt = new msgrpc::RpcContext();
-            ctxt->service_resolver_ = &SERVICE_RESOLVER::instance();
-
-            msgrpc::Cell<RSP> *result_cell = do_run(req, *ctxt);
-
-            assert(result_cell != nullptr && "should not return nullptr cell from SI");
-            result_cell->set_binded_context(*ctxt);
-            ctxt->release_list_.remove(result_cell);
-
-            return result_cell;
-        }
-
-        virtual msgrpc::Cell<RSP> *do_run(const REQ &req, msgrpc::RpcContext& ctxt) = 0;
-    };
-
-
     template<typename SERVICE_RESOLVER, typename SI_FUNCTION, typename REQ>
     auto run_si(SI_FUNCTION& si_func, const REQ &req) -> function_result_type<SI_FUNCTION> {
         typedef typename std::remove_pointer<function_result_type<SI_FUNCTION>>::type RSP;
@@ -46,24 +27,7 @@ namespace msgrpc {
     auto run_si(SI_FUNCTION& si_func, const REQ &req) -> function_result_type<SI_FUNCTION> {
         return run_si<DefaultServiceResolver>(si_func, req);
     }
+
 }
-
-
-
-#define DEFINE_SI(SI_NAME_, REQ_, REQ_VAR_, RSP_)                                                    \
-    struct SI_NAME_ : msgrpc::SIBase<REQ_, RSP_> {                                                   \
-        virtual msgrpc::Cell<RSP_>* do_run(const REQ_& REQ_VAR_, msgrpc::RpcContext& ctxt) override; \
-    };                                                                                               \
-    msgrpc::Cell<RSP_>* SI_NAME_::do_run(const REQ_& REQ_VAR_, msgrpc::RpcContext& ctxt)
-
-
-
-#define DEFINE_SI_WITH_RESOLVER(SI_NAME_, REQ_, REQ_VAR_, RSP_, SERVICE_RESOLVER)                    \
-    struct SI_NAME_ : msgrpc::SIBase<REQ_, RSP_, SERVICE_RESOLVER> {                                 \
-        virtual msgrpc::Cell<RSP_>* do_run(const REQ_& REQ_VAR_, msgrpc::RpcContext& ctxt) override; \
-    };                                                                                               \
-    msgrpc::Cell<RSP_>* SI_NAME_::do_run(const REQ_& REQ_VAR_, msgrpc::RpcContext& ctxt)
-
-
 
 #endif //MSGRPC_SI_BASE_H
